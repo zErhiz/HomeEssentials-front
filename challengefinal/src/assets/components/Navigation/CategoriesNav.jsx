086b7c26d9861838f7a414b9d54ca3cd24495d1a
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-06 10:45:09 -0300

    second

--- challengefinal/src/assets/components/Navigation/CategoriesNav.jsx
+++ challengefinal/src/assets/components/Navigation/CategoriesNav.jsx
@@ -21,7 +21,7 @@ const CategoriesNav = () => {
 }, [])
 
   return (
-    <div className=' hidden lg:block h-[40px] bg-[#EDECEC] flex items-center content-center'>
+    <div className=' hidden lg:block h-[40px] bg-[#EDECEC] lg:flex items-center content-center'>
     <div>
       <ul className="flex justify-start px-12 space-x-4 items-center content-center">
       {categories.map((cat) => (
