5157b191e2ac8e4f5eb4d408b63b9992a93ee9d8
Tomas Salazar <tomas98salazar@gmail.com>
2023-06-06 12:45:56 -0300

    Admin panel mockup

--- challengefinal/src/router/router.jsx
+++ challengefinal/src/router/router.jsx
@@ -4,6 +4,7 @@ import Index from "../assets/pages/Index";
 import Parte1 from "../assets/pages/parte1";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
+import AdminPanel from "../pages/AdminPanel";
 const routes = createBrowserRouter([
     {
       path: "/",
@@ -11,8 +12,9 @@ const routes = createBrowserRouter([
       children: [
         { path: "/", element: <Index /> },
         { path: "/2", element: <Parte1 /> },
-        {path:"/signin", element:<Signin/>},
-        {path:"/signup", element:<Signup/>},
+        { path:"/signin", element:<Signin/>},
+        { path:"/signup", element:<Signup/>},
+        { path:"/admin", element:<AdminPanel/>},
     ],
 },
 ]);
