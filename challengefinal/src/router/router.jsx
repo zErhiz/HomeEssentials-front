db912b0c8d7ef79ee38472ccb07080de3e678573
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-06 10:18:50 -0300

    first

--- challengefinal/src/router/router.jsx
+++ challengefinal/src/router/router.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../assets/layout/layout";
 import Index from "../assets/pages/Index";
 import Parte1 from "../assets/pages/parte1";
+import Home from "../assets/pages/Home"
 const routes = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
       children: [
-        { path: "/", element: <Index /> },
+        { path: "/", element: <Home /> },
         { path: "/2", element: <Parte1 /> },
     ],
 },
