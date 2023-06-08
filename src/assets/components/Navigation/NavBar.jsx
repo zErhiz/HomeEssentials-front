2991d508d8f46a0129b2360ce3b897f86a8907fe
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2023-06-08 12:29:22 -0300

    pagina login, comenzando por model de favoritos (#15)

--- src/assets/components/Navigation/NavBar.jsx
+++ src/assets/components/Navigation/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link as Anchor } from 'react-router-dom';
 import MiddleNavBar from './MiddleNavBar';
 import SearchAndLogoNavbar from './SearchAndLogoNavbar';
 import CategoriesNav from './CategoriesNav';
+
+
 const NavBar = () => {
   return (
     <>
-       <MiddleNavBar /> 
+      <MiddleNavBar />
       <SearchAndLogoNavbar />
         <CategoriesNav />  
     </>
