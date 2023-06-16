075c45a8949dafad184ce0a60e07e0e09c241bc5
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-16 10:22:02 -0300

    fix

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 
 let apiUrl = 'http://localhost:8000/'
 
-/* if (process.env.NODE_ENV === "production") {
+ if (process.env.NODE_ENV === "production") {
     apiUrl = import.meta.env.VITE_API
-} */
+} 
 
 export default apiUrl
